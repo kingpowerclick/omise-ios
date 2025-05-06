8ba7b81aa45bfc95f25a62d8c7b0c12b999f96d4
Wipoo Shinsirikul <749739+mylifeasdog@users.noreply.github.com>
2025-05-06 15:17:37 +0700

    Change OmisedSDK to static library

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "OmiseSDK",
-            type: .dynamic,
+//            type: .dynamic,
             targets: ["OmiseSDK"]
         )
     ],
